0078412c6d16c2bf6cdf8c3229b92c2d1b1c8a32
jk <joern.kreutel@beuth-hochschule.de>
2019-12-05 20:09:33 +0100

    initial creation

---
+++ R/partitioning.R
@@ -0,0 +1,8 @@
+#' A converter function for partitionings
+#'
+#' creates a partitioning from a list of values
+#' @param list the list of values to be partitioned
+#' @param num the number of partitions to create
+partitioning <- function(list=NULL,num=-1) {
+	return(cluster::pam(list,num))
+}
