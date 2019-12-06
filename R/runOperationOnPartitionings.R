b54fc9d7d8b07a81f3b935c31da3e095f57a67e3
jk <joern.kreutel@beuth-hochschule.de>
2019-12-06 12:20:17 +0100

    change string comparison...

--- R/runOperationOnPartitionings.R
+++ R/runOperationOnPartitionings.R
@@ -11,7 +11,9 @@ runOperationOnPartitionings <- function(op=Null,p1=NULL,n1=-1,p2=NULL,n2=-1,opti
 	arg1 <- cluster::pam(p1,n1)
 	arg2 <- cluster::pam(p2,n2)
 
-	if (op == "clue:cl_dissimilarity") {
+	opstr <- as.character(op)
+
+	if (opstr == as.character("clue:cl_dissimilarity")) {
         result <- clue:cl_dissimilarity(arg1,arg2,ifelse(options != null,options[[1]],NULL))
 	}
 	else {
